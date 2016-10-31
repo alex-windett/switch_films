4b0ea4ec3d384a7cec17668759526509496a66a8
Alex Windett <windettalex@gmail.com>
2016-10-31 08:03:27 +0000

    pulling in vimeo videos and setting to HTML\

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 
 $(document).ready(function() {
+    const vimeoAPI = {
+        getVideos: 'https://api.vimeo.com/me/videos?access_token=42315fec2bf964ec800311df5f221383'
+    }
+
+    // VIMEO API ENDPOINT = api.vimeo.com/me/videos?access_token=42315fec2bf964ec800311df5f221383
+
     $('.section').click(function() {
         $('body').toggleClass('isZoomed')
         $(this).find('.button').toggleClass('button__isClicked');
@@ -14,5 +20,23 @@ $(document).ready(function() {
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
-	})
+	});
+
+    const appendVideosToGrid = function(data) {
+        data.forEach( function(video, index) {
+            console.log(video, 'index ' + index)
+            const embedHTML = '<div class="image__wrapper column">' + video.embed.html + '</div>';
+            $('.video-row').append(embedHTML);
+        } )
+    }
+
+    $.get(vimeoAPI.getVideos)
+        .done(function(data) {
+            // console.log(data, ' done')
+            appendVideosToGrid(data.data);
+        })
+        .fail(function(data) {
+            console.log(data, ' fail')
+        })
+
 })
