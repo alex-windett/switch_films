56af3e5a21c192f4802b9068702bff354b21dbbf
Alex Windett <windettalex@gmail.com>
2016-11-02 20:02:33 +0000

    adding in dev script and holding page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('holding-page/', { title: 'Express' });
 });
 
 router.get('/switch-films-demo', function(req, res) {
